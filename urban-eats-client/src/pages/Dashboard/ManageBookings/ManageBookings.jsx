c04ee31491f7b58e56957c8ba36fc598616408ce
Hazrat16 <hazrat17016@gmail.com>
2024-05-18 13:57:24 +0600

    admin can see all the offers from admin site now and can perform delete bookings

--- urban-eats-client/src/pages/Dashboard/ManageBookings/ManageBookings.jsx
+++ urban-eats-client/src/pages/Dashboard/ManageBookings/ManageBookings.jsx
@@ -1,56 +1,84 @@
-import React, { useState } from 'react';
-import TableBookings from './TableBookings';
+import React from "react";
+import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
+import useAxiosSecureLocal from "../../../hooks/useAxiosSecureLocal";
+import useBookings from "../../../hooks/useBookings";
+import "./style.css";
 
+const ManageBookings = () => {
+  const [bookings, refetch] = useBookings();
+  const axiosSecure = useAxiosSecureLocal();
 
-const movies = [
-    {
-      name: "Vadivasal",
-      price: 120,
-      occupied: [20, 21, 30, 1, 2, 8]
-    },
-    {
-      name: "Valimai",
-      price: 150,
-      occupied: [9, 41, 35, 11, 65, 26]
-    },
-    {
-      name: "Beast",
-      price: 200,
-      occupied: [37, 25, 44, 13, 2, 3]
-    },
-    {
-      name: "Doctor",
-      price: 100,
-      occupied: [10, 12, 50, 33, 28, 47]
-    },
-    {
-      name: "Ponniyin Selvan-The Beginning",
-      price: 200,
-      occupied: [7, 25, 4, 13, 20, 31]
-    },
-    {
-      name: "Vikram",
-      price: 180,
-      occupied: [3, 5, 40, 3, 20, 21]
-    }
-  ];
-  
- const ManageBookings = () => {
-    
-  const [selectedMovie, setSelectedMovie] = useState(movies[0]);
-  const [selectedSeats, setSelectedSeats] = useState([]);
+  const handleDeleteBooking = (id) =>{
+    console.log("deleted bookings id",id)
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/bookings/${id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  }
   return (
-    
-    <>
-    <TableBookings
-    movie={selectedMovie}
-    selectedSeats={selectedSeats}
-    onSelectedSeatsChange={(selectedSeats) =>
-      setSelectedSeats(selectedSeats)
-    }
-    />
-    </>
-  )
-}
+    <div>
+      <div className="flex flex-col justify-center content-center my-4">
+        <h2 className="text-3xl text-center">Bookings</h2>
+        <h2 className="text-xl text-center">
+          Total Number of Users: {bookings.length}
+        </h2>
+      </div>
+      <div className="overflow-x-auto border-t-lg">
+      <table className="min-w-full divide-y divide-gray-200">
+      <thead className="bg-gray-50">
+        <tr>
+          <th className="px-6 py-3 text-center text-xs font-bold text-white  uppercase tracking-wider"></th>
+          <th className="px-6 py-3 text-center text-xs font-bold text-white  uppercase tracking-wider">Selected Date</th>
+          <th className="px-6 py-3 text-center text-xs font-bold text-white  uppercase tracking-wider">Meal Type</th>
+          <th className="px-6 py-3 text-center text-xs font-bold text-white  uppercase tracking-wider">Phone No</th>
+          <th className="px-6 py-3 text-center text-xs font-bold text-white  uppercase tracking-wider">Table No</th>
+          <th className="px-6 py-3 text-center text-xs font-bold text-white  uppercase tracking-wider">Action</th>
+        </tr>
+      </thead>
+      <tbody className="bg-white divide-y divide-gray-200 text-center">
+        {bookings.map((booking, index) => (
+          <tr key={booking._id}>
+            <th className="px-6 py-4 whitespace-nowrap">{index + 1}</th>
+            <td className="px-6 py-4 whitespace-nowrap">{booking.selectedDate}</td>
+            <td className="px-6 py-4 whitespace-nowrap">{booking.mealType}</td>
+            <td className="px-6 py-4 whitespace-nowrap">{booking.phoneNumber}</td>
+            <td className="px-6 py-4 whitespace-nowrap">
+              {booking.bookedTable.join(', ')}
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap">
+                  <button
+                    onClick={() => handleDeleteBooking(booking._id)}
+                    className="btn btn-ghost btn-lg"
+                  >
+                    <FaTrashAlt className="text-red-600"></FaTrashAlt>
+                  </button>
+                </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+      </div>
+    </div>
+  );
+};
 
 export default ManageBookings;
--- urban-eats-client/src/pages/Dashboard/ManageBookings/TableBookings.jsx
+++ urban-eats-client/src/pages/Dashboard/ManageBookings/TableBookings.jsx
@@ -1,53 +1,62 @@
-import clsx from "clsx";
 import React from "react";
-import './style.css';
+import "./style.css";
 
-const seats = Array.from({ length: 5 * 6 }, (_, i) => i);
 
-const TableBookings = ({ movie, selectedSeats, onSelectedSeatsChange }) => {
-  const handleSelectedState = (seat) => {
-    console.log(seat+1)
-    const isSelected = selectedSeats.includes(seat);
-    if (isSelected) {
-      onSelectedSeatsChange(
-        selectedSeats.filter((selectedSeat) => selectedSeat !== seat)
-      );
-    } else {
-      onSelectedSeatsChange([...selectedSeats, seat]);
-    }
-  };
+const Bookings = ({ movie, selectedSeats, onSelectedSeatsChange }) => {
+  
 
   return (
-    <div className="Cinema">
-      <div className="screen" />
-      Choose your table...
-
-      <div className="seats">
-        {seats.map((seat) => {
-          const isSelected = selectedSeats.includes(seat);
-          const isOccupied = movie.occupied.includes(seat);
-          return (
-            <span
-              tabIndex="0"
-              key={seat}
-              className={clsx(
-                "seat",
-                isSelected && "selected",
-                isOccupied && "occupied"
-              )}
-              onClick={isOccupied ? null : () => handleSelectedState(seat)}
-              onKeyUp={
-                isOccupied
-                  ? null
-                  : (e) => {
-                      if (e.key === "Enter") {
-                        handleSelectedState(seat);
-                      }
-                    }
-              }
-            />
-          );
-        })}
+    <div>
+      <div className="flex flex-col justify-center content-center my-4">
+        <h2 className="text-3xl text-center">All Users</h2>
+        <h2 className="text-xl text-center">
+          Total Number of Users: {users.length}
+        </h2>
+      </div>
+      <div className="overflow-x-auto border-t-lg">
+        <table className="table table-zebra w-full">
+          <thead className="bg-[#197BFD] h-20 text-white text-center ">
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody className=" text-center ">
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  {user.role === "admin" ? (
+                    "Admin"
+                  ) : (
+                    <button
+                      onClick={() => handleMakeAdmin(user)}
+                      className="btn btn-lg bg-orange-500"
+                    >
+                      <FaUsers
+                        className="text-white 
+                                    text-2xl"
+                      ></FaUsers>
+                    </button>
+                  )}
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleDeleteUser(user)}
+                    className="btn btn-ghost btn-lg"
+                  >
+                    <FaTrashAlt className="text-red-600"></FaTrashAlt>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
