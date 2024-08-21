15a7771d140c6c524cbbf9c1ec1a18b458075ecf
hazrat <hazrat17016@gmail.com>
2024-08-21 10:14:48 +0600

    Checking lint in js file which are changed

--- urban-eats-client/.eslintrc.cjs
+++ urban-eats-client/.eslintrc.cjs
@@ -1,23 +1,30 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, node: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest", // Or a specific ECMAScript version like 2020
+    sourceType: "module", // Ensure this is set to 'module' to support import/export syntax
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-    'prettier' 
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', 'import'],
+  plugins: ["react-refresh", "import", "prettier"],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react/prop-types': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true }
+    "react/jsx-no-target-blank": "off",
+    "react/prop-types": "off",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
     ],
+    "prettier/prettier": "error",
   },
 };
--- urban-eats-client/src/Routes/AdminRoute.jsx
+++ urban-eats-client/src/Routes/AdminRoute.jsx
@@ -6,14 +6,14 @@ const AdminRoute = ({ children }) => {
   const { user, loading } = useAuth();
   console.log(loading);
   const [isAdmin, isAdminLoading] = useAdmin();
-  const location =                    useLocation();
+  const location =                                      useLocation();
 
   if (loading || isAdminLoading) {
     return <progress className="progress w-56"></progress>;
   }
 
   if (user && isAdmin) {
-    return children; 
+    return children;
   }
   return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 };
